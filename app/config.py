b50f5b5dc4154a2efae1871bdfee83d3cbe3bf25
Brayden WSL <brayway05@gmail.com>
2024-11-27 23:12:19 -0700

    Fixed merge conflict

--- app/config.py
+++ app/config.py
@@ -1,9 +1,5 @@
 import os
 
-<<<<<<< HEAD
-MODEL_NAME = os.getenv("MODEL_NAME", "meta-llama/Llama-3.2-3B-Instruct")
-=======
 GENERATOR_MODEL_NAME = os.getenv("GENERATOR_MODEL_NAME", "meta-llama/Llama-3.2-3B-Instruct")
 ENCODER_MODEL_NAME = os.getenv("ENCODER_MODEL_NAME", "sentence-transformers/all-MiniLM-L6-v2")
->>>>>>> accea9112dc7623789db631088af8685ee87d0a8
 HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
--- app/main.py
+++ app/main.py
@@ -2,16 +2,9 @@ from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 import os
-<<<<<<< HEAD
-from app.config import MODEL_NAME, HUGGINGFACE_TOKEN
-import transformers
-from app.model import get_model
-from app.inference import generate_text
-=======
 from app.config import GENERATOR_MODEL_NAME, ENCODER_MODEL_NAME, HUGGINGFACE_TOKEN
 from app.model import get_generator_model, get_encoder_model
 from app.inference import generate_text, encode_text
->>>>>>> accea9112dc7623789db631088af8685ee87d0a8
 from pydantic import BaseModel
 from huggingface_hub import login
 
@@ -33,13 +26,6 @@ class LLMResponse(BaseModel):
 
 @app.on_event("startup")
 def startup_event():
-<<<<<<< HEAD
-    print("Logging in to Hugging Face Hub...")
-    login(HUGGINGFACE_TOKEN)
-    print(f"Loading model: {MODEL_NAME}...")
-    get_model()
-    print("Model is ready and cached!")
-=======
     print("Logging into Hugging Face Hub...")
     login(HUGGINGFACE_TOKEN)
     print("Logged In!")
@@ -47,7 +33,6 @@ def startup_event():
     get_generator_model()
     get_encoder_model()
     print("Models are ready and cached!")
->>>>>>> accea9112dc7623789db631088af8685ee87d0a8
 
 @app.get("/generate")
 def generate_response(request: LLMRequest) -> LLMResponse:
