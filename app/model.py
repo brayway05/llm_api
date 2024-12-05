b26bf8a913907190f0d922e8c51d54f312143ea0
root <root@BRAYDENS-DESKTOP>
2024-12-04 23:27:18 -0700

    Distilgpt2

--- app/model.py
+++ app/model.py
@@ -9,6 +9,7 @@ encoder_tokenizer = None
 def get_generator_model():
     global generator_model, generator_tokenizer
     if generator_model is None or generator_tokenizer is None:
+        
         generator_model = AutoModelForCausalLM.from_pretrained(GENERATOR_MODEL_NAME)
         generator_tokenizer = AutoTokenizer.from_pretrained(GENERATOR_MODEL_NAME)
     return generator_model, generator_tokenizer
