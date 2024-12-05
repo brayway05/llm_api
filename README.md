ee4970bf8e5059cc1b6ba18cd55d582ef254164b
brayway05 <brayway05@gmail.com>
2024-12-04 23:00:21 -0700

    env was not working as intended

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # llm_api
 FastAPI server for RAG project. Contains API for calling LLM inference running on my PC with GPU's.
 
-`docker build -t llm_api .`
-
 Need a HUGGINGFACE_TOKEN in Dockerfile file to get access to llama 3.2
 `docker build --build-arg HUGGINGFACE_TOKEN="Token Here" -t llm-api .`
 
-`docker run -p 5002:5002 llm-api`
+`docker run --name llm-api -p 5002:5002 llm-api`
--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 
-GENERATOR_MODEL_NAME = os.getenv("GENERATOR_MODEL_NAME", "meta-llama/Llama-3.2-3B-Instruct")
-ENCODER_MODEL_NAME = os.getenv("ENCODER_MODEL_NAME", "sentence-transformers/all-MiniLM-L6-v2")
+# GENERATOR_MODEL_NAME = "meta-llama/Llama-3.2-3B-Instruct"
+GENERATOR_MODEL_NAME = "distilbert/distilbert-base-uncased-distilled-squad"
+ENCODER_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
